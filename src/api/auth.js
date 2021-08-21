1a215ee52a6219c330f23d6074e79f5f4bf9fe65
Charlie <wl00606352@gmail.com>
2021-08-21 19:22:26 +0800

    login api

---
+++ src/api/auth.js
@@ -0,0 +1,35 @@
+export const registerAPI = async ({ email, password }) => {
+  return await fetch(
+    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+      process.env.REACT_APP_G_API,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        password,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+};
+
+export const loginAPI = async ({ email, password }) => {
+  return await fetch(
+    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+      process.env.REACT_APP_G_API,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        password,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+};
