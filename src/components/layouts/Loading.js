d4f52e13871b827ba8687ebdfb6d1e0941ec267f
Charlie <wl00606352@gmail.com>
2021-08-21 17:13:52 +0800

    feat: lazy loading

---
+++ src/components/layouts/Loading.js
@@ -0,0 +1,13 @@
+import { Stack, Skeleton } from '@chakra-ui/react';
+
+const Loading = () => {
+  return (
+    <Stack>
+      <Skeleton height="20px" />
+      <Skeleton height="20px" />
+      <Skeleton height="20px" />
+    </Stack>
+  );
+};
+
+export default Loading;
--- src/router/Index.js
+++ src/router/Index.js
@@ -1,4 +1,4 @@
-import { Fragment } from 'react';
+import { Fragment, lazy, Suspense } from 'react';
 import { Link } from '@chakra-ui/react';
 import {
   Switch as RouteSwitch,
@@ -7,39 +7,42 @@ import {
   Redirect,
   Link as ReachLink,
 } from 'react-router-dom';
+import Loading from '../components/layouts/Loading';
 
-import Header from '../components/Menu/Header';
-import Pets from '../pages/Pets';
-import Login from '../pages/Login';
+const Header = lazy(() => import('../components/Menu/Header'));
+const Pets = lazy(() => import('../pages/Pets'));
+const Login = lazy(() => import('../pages/Login'));
 
 const Index = () => {
   const location = useLocation();
 
   return (
     <Fragment>
-      {/* <Header /> */}
-      <RouteSwitch>
-        <Route path="/records">
-          <p>here you are at records? {location.pathname}</p>
-        </Route>
-        <Route path="/pets">
-          <Pets />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/:noFound">
-          <p>
-            No page found!
-            <Link color="teal.500" as={ReachLink} to="/">
-              Take me back!
-            </Link>
-          </p>
-        </Route>
-        <Route path="/" exact>
-          <Redirect to="/login" />
-        </Route>
-      </RouteSwitch>
+      <Suspense fallback={<Loading />}>
+        {/* <Header /> */}
+        <RouteSwitch>
+          <Route path="/records">
+            <p>here you are at records? {location.pathname}</p>
+          </Route>
+          <Route path="/pets">
+            <Pets />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/:noFound">
+            <p>
+              No page found!
+              <Link color="teal.500" as={ReachLink} to="/">
+                Take me back!
+              </Link>
+            </p>
+          </Route>
+          <Route path="/" exact>
+            <Redirect to="/login" />
+          </Route>
+        </RouteSwitch>
+      </Suspense>
     </Fragment>
   );
 };
