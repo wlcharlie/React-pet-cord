77bbf055e502dcf20702cc606a9322ef2ab29af0
Charlie <wl00606352@gmail.com>
2021-08-22 19:14:06 +0800

    fix: refactor and returning the res and object

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,51 +1,91 @@
-export const registerAPI = async ({ email, password }) => {
-  return await fetch(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
-      process.env.REACT_APP_G_API,
-    {
+const db = 'https://pet-cord.herokuapp.com/api';
+
+export const registerAPI = async ({ email, password, username }) => {
+  try {
+    const res = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+        process.env.REACT_APP_G_API,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email,
+          password,
+          returnSecureToken: true,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    const data = await res.json();
+    const userRes = await fetch(db + '/users', {
       method: 'POST',
       body: JSON.stringify({
+        name: username,
         email,
-        password,
-        returnSecureToken: true,
+        UID: data.localId,
       }),
       headers: {
         'Content-Type': 'application/json',
       },
-    }
-  );
+    });
+    const user = await userRes.json();
+    return { res, data, user };
+  } catch (error) {
+    return error;
+  }
 };
 
 export const loginAPI = async ({ email, password }) => {
-  return await fetch(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
-      process.env.REACT_APP_G_API,
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        email,
-        password,
-        returnSecureToken: true,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  );
+  try {
+    const res = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+        process.env.REACT_APP_G_API,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email,
+          password,
+          returnSecureToken: true,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    const data = await res.json();
+    const userRes = await fetch(db + '/users/' + data.localId, {
+      method: 'GET',
+    });
+    const user = await userRes.json();
+    return { res, data, user };
+  } catch (error) {
+    return error;
+  }
 };
 
 export const findAccountAPI = async token => {
-  return await fetch(
-    'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=' +
-      process.env.REACT_APP_G_API,
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        idToken: token,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  );
+  try {
+    const res = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=' +
+        process.env.REACT_APP_G_API,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          idToken: token,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    const data = await res.json();
+    const userRes = await fetch(db + '/users/' + data.users[0].localId, {
+      method: 'GET',
+    });
+    const user = await userRes.json();
+    return { res, data, userRes, user };
+  } catch {
+    return false;
+  }
 };
