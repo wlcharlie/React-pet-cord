1bd7fa8caa65c351de008eaffebc9dc230176890
Charlie <wl00606352@gmail.com>
2021-08-24 23:33:06 +0800

    adding unix convert to date format

---
+++ src/utils/convertToDate.js
@@ -0,0 +1,11 @@
+export const formatDate = date => {
+  let formatted_date =
+    date.getFullYear() +
+    '-' +
+    (date.getMonth() + 1 > 10
+      ? date.getMonth() + 1
+      : '0' + (date.getMonth() + 1)) +
+    '-' +
+    (date.getDate() > 10 ? date.getDate() : '0' + date.getDate());
+  return formatted_date;
+};
