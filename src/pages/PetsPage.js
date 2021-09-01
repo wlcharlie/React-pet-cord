da0d7a03d5c31cb12e8d70e638fa9f923486adc1
Charlie <wl00606352@gmail.com>
2021-09-02 00:00:19 +0800

    refactor: pets page removing no need line

--- src/pages/PetsPage.js
+++ src/pages/PetsPage.js
@@ -1,23 +1,12 @@
-import { Fragment, useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { Fragment } from 'react';
 import PetsContainer from '../components/layouts/PetsContainer';
 import PetCards from '../components/Pets/PetCards';
 import AddPetMenu from '../components/Pets/AddPetMenu';
-import PetCardLoading from '../components/layouts/PetCardLoading';
 
 const PetsPage = () => {
-  // const petData = useSelector(state => state.pets);
-  // const [pets, setPet] = useState(null);
-
-  // useEffect(() => {
-  //   setPet(petData.map(e => <PetCard key={e._id} data={e} />));
-  // }, [petData]);
-
   return (
     <Fragment>
       <PetsContainer>
-        {/* {!pets && <PetCardLoading />}
-        {pets && pets} */}
         <PetCards />
       </PetsContainer>
       <AddPetMenu />
